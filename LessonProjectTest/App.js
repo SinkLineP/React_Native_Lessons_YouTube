be4605a9aa12e112871069c8cf29770cd0da8823
SinkLine_P <popovartem505@gmail.com>
2022-08-10 10:25:26 +0300

    Two types creating CSS styles

--- LessonProjectTest/App.js
+++ LessonProjectTest/App.js
@@ -1,35 +1,19 @@
-import {StyleSheet, Text, SafeAreaView, StatusBar, Button, Alert, Image, TouchableWithoutFeedback} from 'react-native';
+import {StyleSheet, Text, SafeAreaView, StatusBar} from 'react-native';
 
 export default function App() {
-  const handleTextPress = () => console.log("Test click text");
-  const handleButtonPress = () => Alert.alert("Опрос", "Ты нуб?", [
-    {text: "Да", onPress: () => console.log("Yes Button")},
-    {text: "Нет", onPress: () => console.log("No Button")},
-  ]);
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} style={styles.text} onPress={handleTextPress}>Hello Artem, {'\n'}Hello Misha</Text>
-      <Button title={'Info'} color="blue" onPress={handleButtonPress}/>
-      {/*<Image source={require("./Images/anime01.jpg")}/>*/}
-      <TouchableWithoutFeedback onPress={handleButtonPress}>
-        <Image blurRadius={4} source={{
-          width: 100,
-          height: 100,
-          uri: "https://i.pinimg.com/originals/af/1c/30/af1c30d6d881d9447dec06149f61d2f9.png"
-        }}/>
-      </TouchableWithoutFeedback>
-
+      <Text style={simpleStyle}>Hello</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
 
+const simpleStyle = {backgroundColor: "green", color: "yellow"}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  text: {
-    color: 'gray',
   }
 });
