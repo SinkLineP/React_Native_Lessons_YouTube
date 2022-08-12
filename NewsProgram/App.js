076ee6d985bf464899beeae61e2a106398eb43ac
SinkLine_P <popovartem505@gmail.com>
2022-08-12 11:35:26 +0300

    ...init... | News Program

---
+++ NewsProgram/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {StyleSheet, View, StatusBar } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
